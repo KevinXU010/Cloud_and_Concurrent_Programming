6bfe199700cd7eb975a1f0ab812dfd0b9139bdb6
KevinXU010 <xuyky010@mymail.unisa.edu.au>
2025-10-29 21:21:36 +1030

    Implement pickup and drive with sleeps, add throws InterruptedException.

--- Assignment 2 Concurrent A2.2g/src/nuber/students/Driver.java
+++ Assignment 2 Concurrent A2.2g/src/nuber/students/Driver.java
@@ -1,10 +1,14 @@
 package nuber.students;
 
 public class Driver extends Person {
+	
+	private Passenger currentPassenger;
+
 
 	
 	public Driver(String driverName, int maxSleep)
 	{
+		super(driverName, maxSleep);
 	}
 	
 	/**
@@ -14,8 +18,11 @@ public class Driver extends Person {
 	 * @param newPassenger Passenger to collect
 	 * @throws InterruptedException
 	 */
-	public void pickUpPassenger(Passenger newPassenger)
+	public void pickUpPassenger(Passenger newPassenger)throws InterruptedException
 	{
+		this.currentPassenger = newPassenger;
+        
+        Thread.sleep((int) (Math.random() * maxSleep));
 	}
 
 	/**
@@ -24,7 +31,10 @@ public class Driver extends Person {
 	 * 
 	 * @throws InterruptedException
 	 */
-	public void driveToDestination() {
+	public void driveToDestination() throws InterruptedException {
+		Passenger p = this.currentPassenger;
+        if (p == null) return; 
+        Thread.sleep(p.getTravelTime());
 	}
 	
 }
